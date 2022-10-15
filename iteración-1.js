8167c7143c8446afb3401595929f861d94aa7a3e
mariosepulveda3 <mariosepul3@gmail.com>
2022-10-15 12:37:35 +0200

    DONE, iteración 1

--- iteración-1.js
+++ iteración-1.js
@@ -1,6 +1,23 @@
 // 1.1 Añade un botón a tu html con el id btnToClick y en tu javascript añade el 
 // evento click que ejecute un console log con la información del evento del click
 
+const btnToClick$$ = document.querySelector('#btnToClick');
+
+btnToClick$$.addEventListener('click', function(event) {
+    console.log(event);
+});
+
 // 1.2 Añade un evento 'focus' que ejecute un console.log con el valor del input.
 
-// 1.3 Añade un evento 'input' que ejecute un console.log con el valor del input.
+const focus$$ = document.querySelector('.focus');
+focus$$.addEventListener('focus', function() {
+    console.log(focus$$.value);
+});
+
+// 1.3 Añade un evento 'input' que ejecute un console.log con el 
+// valor del input.
+
+const value$$ = document.querySelector('.value');
+value$$.addEventListener('input', function() {
+    console.log(value$$.value);
+})
